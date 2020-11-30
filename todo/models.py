dd2242d18936a52542133dac8bd3f0581d1b19b1
adam.burczy <adam@burczy.com>
2020-11-30 19:13:50 +0100

    model changes

--- todo/models.py
+++ todo/models.py
@@ -8,14 +8,14 @@ class Task(models.Model):
     notes = models.TextField(max_length=150)       # TypeError: expected string or bytes-like object
     created = models.DateField(auto_now_add=True)  # add start as, start of task, default = created maybe
     deadline = models.DateField(default=None)      # add status: not started yes, ongoing, finished
-    #STATUS_CHOICES = [
-     #   ('not started yet', 'not started yet'),
-      #  ('ongoing', 'ongoing'),
-       # ('finished', 'finished')
-    #]
-    #status = models.CharField(choices=STATUS_CHOICES,
-     #                         max_length=20,
-      #                        default='ongoing')
+    STATUS_CHOICES = [
+        ('not started yet', 'not started yet'),
+        ('ongoing', 'ongoing'),
+        ('finished', 'finished')
+    ]
+    status = models.CharField(choices=STATUS_CHOICES,
+                              max_length=20,
+                              default='ongoing')
 
     def __str__(self):
         return self.title
